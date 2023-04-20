1f703105f2f48117a5c6c3a0f5bf103685686140
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2023-04-20 19:58:20 +0530

    Update Auth.js
    Signed-off-by: KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>

--- src/Auth.js
+++ src/Auth.js
@@ -8,8 +8,8 @@ const Auth = ({setsignout,setAuthenticate}) => {
     <Box sx={{height:'100vh',width:'100vw',display:'flex',justifyContent:'center',alignItems:'center',color:'white'}} >
    <Paper sx={{p:5}}>
 
-Welcome to 24/7 Entertainment app
-Please Sign IN to Continue
+Welcome to 24/7 Entertainment 
+Please Sign IN to Continue 
 
    </Paper>
 
